c8185b0bc5864ad17c3fb3f9c17e781c396b0c93
Timothy Molina <rithium234@hotmail.com>
2021-06-30 03:06:16 -0400

    fixed safari markup

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
         var last_scroll_top = 0;
         window.addEventListener('scroll', function () {
             let scroll_top = window.scrollY;
-            if (scroll_top < last_scroll_top) {
+            if (scroll_top < last_scroll_top || window.pageYOffset < 50) {
                 el_autohide.classList.remove('scrolled-down');
                 el_autohide.classList.add('scrolled-up');
             }
